0bc6c79ef50da6d9f0e92ab8c07ba6a91fce6805
Ykrop3000 <grisha.andreev.03@gmail.com>
2021-06-17 21:43:20 +0300

    Update database config

--- config/database.js
+++ config/database.js
@@ -14,6 +14,7 @@
 //   },
 // });
 const parse = require("pg-connection-string").parse;
+const config = parse(process.env.DATABASE_URL);
 
 module.exports = ({ env }) => ({
   defaultConnection: "default",
@@ -22,15 +23,18 @@ module.exports = ({ env }) => ({
       connector: "bookshelf",
       settings: {
         client: "postgres",
-        host: env("DATABASE_HOST", "localhost"),
-        port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "postgres"),
-        username: env("DATABASE_USERNAME", "postgres"),
-        password: env("DATABASE_PASSWORD", "root"),
-        schema: env("DATABASE_SCHEMA", "public"), // Not Required
-        ssl: false,
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        username: config.user,
+        password: config.password,
+        ssl: {
+          rejectUnauthorized: false
+        }
       },
-      options: {},
-    },
-  },
+      options: {
+        ssl: true
+      }
+    }
+  }
 });
