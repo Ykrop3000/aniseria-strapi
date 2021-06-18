13ef06e4922a798535de97ed6c2540d701e496e7
Ykrop3000 <grisha.andreev.03@gmail.com>
2021-06-18 12:47:31 +0300

    Update database config

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -16,9 +16,7 @@ module.exports = ({ env }) => ({
         }
       },
       // add this line
-      options: {
-        ssl: env.bool("DATABASE_SSL", false)
-      }
+      options: {}
     }
   }
 });
