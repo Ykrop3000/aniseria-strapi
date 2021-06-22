45b8ac82b044fc272d3662f1b39a62dfb8e32908
Ykrop3000 <grisha.andreev.03@gmail.com>
2021-06-22 22:35:53 +0300

    suka

--- config/database.js
+++ config/database.js
@@ -1,19 +1,39 @@
+// // module.exports = ({ env }) => ({
+// //   defaultConnection: "default",
+// //   connections: {
+// //     default: {
+// //       connector: "bookshelf",
+// //       settings: {
+// //         client: "sqlite",
+// //         filename: env("DATABASE_FILENAME", ".tmp/data.db"),
+// //       },
+// //       options: {
+// //         useNullAsDefault: true,
+// //       },
+// //     },
+// //   },
+// // });
+// const parse = require("pg-connection-string").parse;
+
 // module.exports = ({ env }) => ({
 //   defaultConnection: "default",
 //   connections: {
 //     default: {
 //       connector: "bookshelf",
 //       settings: {
-//         client: "sqlite",
-//         filename: env("DATABASE_FILENAME", ".tmp/data.db"),
+//         client: "postgres",
+//         host: env("DATABASE_HOST", "localhost"),
+//         port: env.int("DATABASE_PORT", 5432),
+//         database: env("DATABASE_NAME", "postgres"),
+//         username: env("DATABASE_USERNAME", "postgres"),
+//         password: env("DATABASE_PASSWORD", "root"),
+//         schema: env("DATABASE_SCHEMA", "public"), // Not Required
+//         ssl: false
 //       },
-//       options: {
-//         useNullAsDefault: true,
-//       },
-//     },
-//   },
+//       options: {}
+//     }
+//   }
 // });
-const parse = require("pg-connection-string").parse;
 
 module.exports = ({ env }) => ({
   defaultConnection: "default",
@@ -22,13 +42,10 @@ module.exports = ({ env }) => ({
       connector: "bookshelf",
       settings: {
         client: "postgres",
-        host: env("DATABASE_HOST", "localhost"),
-        port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "postgres"),
-        username: env("DATABASE_USERNAME", "postgres"),
-        password: env("DATABASE_PASSWORD", "root"),
-        schema: env("DATABASE_SCHEMA", "public"), // Not Required
-        ssl: false
+        host: `/cloudsql/${env("INSTANCE_CONNECTION_NAME")}`,
+        database: env("DATABASE_NAME"),
+        username: env("DATABASE_USERNAME"),
+        password: env("DATABASE_PASSWORD")
       },
       options: {}
     }
